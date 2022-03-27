947255ddaf402b8c222257642c83ab59c41dc6bd
zed <zedsmail@163.com>
2022-03-27 23:52:02 +0800

    feat: delay value and json

---
+++ tools/delayvalue.go
@@ -0,0 +1,92 @@
+package tools
+
+import (
+	"sync"
+	"time"
+)
+
+func NewDelayValue(initValue interface{}, duration time.Duration) *DelayValue {
+	return &DelayValue{
+		currentValue: initValue,
+		lastValue:    initValue,
+		time:         time.Now(),
+		duration:     duration,
+	}
+}
+
+//DelayValue 延迟duration改变值
+type DelayValue struct {
+	currentValue interface{}   //当前
+	time         time.Time     //值变更时间
+	lastValue    interface{}   //值
+	duration     time.Duration //持续观察时间
+	lock         sync.RWMutex
+	recently     bool //是否刚改变
+
+	//下面是锁定状态的设置
+	lockValue    interface{}   //锁定的值
+	lockDuration time.Duration //锁定时间
+	lockAt       time.Time     //锁定开始时间
+}
+
+//Lock 锁定状态
+func (t *DelayValue) Lock(state interface{}, duration time.Duration) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	t.lockValue = state
+	t.lockDuration = duration
+	t.lockAt = time.Now()
+}
+
+// ThisTime 设置当前状态
+func (t *DelayValue) ThisTime(state interface{}) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	if state != t.lastValue {
+		t.time = time.Now()
+		t.lastValue = state
+	} else {
+		if t.time.Add(t.duration).Before(time.Now()) {
+			t.currentValue = state
+			t.time = time.Now()
+
+			//如果锁定就不变改变状态
+			if t.lockValue == nil || t.lockAt.Add(t.lockDuration).Before(time.Now()) {
+				t.lockValue = nil
+				t.recently = true
+			}
+		}
+	}
+}
+
+//Current 获取当前状态
+func (t *DelayValue) Current() interface{} {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	t.recently = false
+
+	//判断锁定状态,如果有就返回锁定的值
+	if t.lockValue != nil && t.lockAt.Add(t.lockDuration).After(time.Now()) {
+		return t.lockValue
+	}
+	t.lockValue = nil
+
+	return t.currentValue
+}
+
+//CurrentBool 获取当前状态
+func (t *DelayValue) CurrentBool() bool {
+	return t.Current().(bool)
+}
+
+//CurrentInt 获取当前状态
+func (t *DelayValue) CurrentInt() int {
+	return t.Current().(int)
+}
+
+//ChangeRecently 是否最近一次的改变,当至少一次通过Current方法获取值后，recently change就变为false
+func (t *DelayValue) ChangeRecently() bool {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	return t.recently
+}
---
+++ tools/json.go
@@ -0,0 +1,50 @@
+package tools
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Json string
+
+func (c *Json) UnmarshalJSON(b []byte) (err error) {
+	*c = Json(b)
+	return nil
+}
+
+func (c Json) MarshalJSON() ([]byte, error) {
+	return []byte(c), nil
+}
+
+func (c Json) Get(name string) (value interface{}, err error) {
+	tmp := make(map[string]interface{})
+	err = json.Unmarshal([]byte(c), &tmp)
+	if err != nil {
+		return
+	}
+	var ok bool
+	for _, n := range strings.Split(name, ".") {
+		if value == nil {
+			value, ok = tmp[n]
+		} else {
+			value, ok = value.(map[string]interface{})[n]
+		}
+		if !ok {
+			err = errors.New(fmt.Sprintf("value of key <%s> not found", n))
+			return
+		}
+	}
+
+	return
+}
+
+func (c Json) GetString(name string) (value string, err error) {
+	v, err := c.Get(name)
+	if err != nil {
+		return
+	}
+	value = v.(string)
+	return
+}
