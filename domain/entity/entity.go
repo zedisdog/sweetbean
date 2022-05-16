6ae782e166500172a8c00fd2a11e699ae2dc5fc0
zed <zedsmail@163.com>
2022-05-16 18:50:48 +0800

    --

--- domain/entity/entity.go
+++ domain/entity/entity.go
@@ -3,7 +3,9 @@ package entity
 type Entity interface {
 	//Assimilate copy field from [from] to self
 	Assimilate(from interface{}) error
-	IsDirty() bool
-	SetDirty(bool)
 	Incarnation(to interface{}) error
+}
+
+type DirtyState struct {
+	Dirty bool `json:"-" gorm:"-"`
 }
