28dfe32d849d1805172a777ca50c846c84c8bf8d
zed <zedsmail@163.com>
2022-06-15 19:42:11 +0800

    --

--- database/gormplugs/autosetid.go
+++ database/gormplugs/autosetid.go
@@ -2,20 +2,38 @@ package gormplugs
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 
 	"github.com/zedisdog/sweetbean/tools/snowflake"
 	"gorm.io/gorm"
 )
 
+//Deprecated:
+//AutoSetIDUsingSnowFlake
 func AutoSetIDUsingSnowFlake(db *gorm.DB) {
 	if db.Statement.Schema != nil && db.Statement.Schema.LookUpField("id") != nil {
-		_, isZero := db.Statement.Schema.LookUpField("id").ValueOf(context.Background(), db.Statement.ReflectValue)
-		if isZero {
-			id, err := snowflake.NextID()
-			if err != nil {
-				panic(err)
+		fmt.Printf("%+v\n", db.Statement.ReflectValue)
+		if db.Statement.ReflectValue.Kind() == reflect.Slice {
+			for _, v := range db.Statement.ReflectValue.Interface().([]reflect.Value) {
+				_, isZero := db.Statement.Schema.LookUpField("id").ValueOf(context.Background(), v)
+				if isZero {
+					id, err := snowflake.NextID()
+					if err != nil {
+						panic(err)
+					}
+					db.Statement.Schema.LookUpField("id").Set(context.Background(), db.Statement.ReflectValue, id)
+				}
 			}
-			db.Statement.Schema.LookUpField("id").Set(context.Background(), db.Statement.ReflectValue, id)
+		} else {
+			_, isZero := db.Statement.Schema.LookUpField("id").ValueOf(context.Background(), db.Statement.ReflectValue)
+			if isZero {
+				id, err := snowflake.NextID()
+				if err != nil {
+					panic(err)
+				}
+				db.Statement.Schema.LookUpField("id").Set(context.Background(), db.Statement.ReflectValue, id)
+			}
 		}
 	}
 }
