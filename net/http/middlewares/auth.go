a6a09535ef0a2a2f448ecb4bbdc69292144a1d3d
zed <zedsmail@163.com>
2022-06-18 18:20:30 +0800

    =--

--- net/http/middlewares/auth.go
+++ net/http/middlewares/auth.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/zedisdog/sweetbean/tools"
@@ -14,6 +15,7 @@ import (
 //	claims: the customer Claims object
 //	key: the key for validate sign
 //	isUserExists: a function to determine if account is exists
+//Deprecated: use BuildAuth instead
 func GenAuthMiddleware(key string, isUserExists func(id interface{}) bool) func(*gin.Context) {
 	return func(c *gin.Context) {
 		var token string
@@ -49,5 +51,69 @@ func GenAuthMiddleware(key string, isUserExists func(id interface{}) bool) func(
 		}
 
 		c.Next()
+	}
+}
+
+//BuildAuth
+//  通过加密用的key和查找用户是否存在的函数构造身份验证中间件，中间件通过header中的Authorization字段或者url中queryString的token字段来获取token
+func BuildAuth(key string, isUserExists func(id uint64) bool) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		var token string
+		if ctx.Request.Header.Get("Authorization") != "" {
+			arr := strings.Split(ctx.Request.Header.Get("Authorization"), " ")
+			if len(arr) < 2 {
+				ctx.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问1"})
+				return
+			}
+			token = arr[1]
+		} else if ctx.Query("token") != "" {
+			token = ctx.Query("token")
+		} else {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问2"})
+			return
+		}
+
+		t, err := tools.Parse(token, []byte(key))
+		if err != nil || !t.Valid {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, map[string]string{"message": "未授权的访问3"})
+			return
+		}
+
+		claims, ok := t.Claims.(jwt.MapClaims)
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "未授权的访问7",
+			})
+			return
+		}
+
+		var jti interface{}
+		jti, ok = claims["jti"]
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "未授权的访问4",
+			})
+			return
+		}
+
+		var id uint64
+		id, err = strconv.ParseUint(jti.(string), 10, 64)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "未授权的访问5",
+			})
+			return
+		}
+
+		if !isUserExists(id) {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "未授权的访问6",
+			})
+			return
+		}
+
+		ctx.Set("id", id)
+		ctx.Set("claims", claims)
+		ctx.Next()
 	}
 }
