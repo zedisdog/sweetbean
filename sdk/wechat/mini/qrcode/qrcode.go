2a792d09e548f68534a6c246d1b058d09c3f203b
zed <zedsmail@163.com>
2022-06-20 01:01:38 +0800

    feat: 微信小程序二维码生成

--- sdk/wechat/mini/qrcode/qrcode.go
+++ sdk/wechat/mini/qrcode/qrcode.go
@@ -3,9 +3,8 @@ package qrcode
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 
+	"github.com/zedisdog/sweetbean/net/http"
 	"github.com/zedisdog/sweetbean/sdk/wechat/mini/auth"
 	"github.com/zedisdog/sweetbean/sdk/wechat/mini/qrcode/response"
 )
@@ -20,29 +19,56 @@ func NewQrCode(a *auth.Auth) *QrCode {
 	}
 }
 
+type Color struct {
+	R int `json:"r"`
+	G int `json:"g"`
+	B int `json:"b"`
+}
+
+type QrCodeUnlimitedOptions struct {
+	Page       string `json:"page"`
+	CheckPath  bool   `json:"check_page"`
+	EnvVersion string `json:"env_version"`
+	Width      int    `json:"width"`
+	AutoColor  bool   `json:"auto_color"`
+	LineColor  Color  `json:"line_color"`
+	IsHyaline  bool   `json:"is_hyaline"`
+}
+
 type QrCode struct {
 	auth *auth.Auth
 }
 
-func (q QrCode) GetUnlimited(sence map[string]string) (r response.QrCodeUnlimited, err error) {
+func (q QrCode) GetUnlimited(sence map[string]string, setters ...func(*QrCodeUnlimitedOptions)) (r response.QrCodeUnlimited, err error) {
+	options := QrCodeUnlimitedOptions{
+		Page:       "",
+		CheckPath:  true,
+		EnvVersion: "release",
+		Width:      430,
+		AutoColor:  false,
+		LineColor: Color{
+			G: 0,
+			R: 0,
+			B: 0,
+		},
+		IsHyaline: false,
+	}
+	for _, set := range setters {
+		set(&options)
+	}
 	token, err := q.auth.GetAccessToken()
 	if err != nil {
 		return
 	}
-	resp, err := http.Get(fmt.Sprintf(qrCodeUnlimited, token))
+	resp, err := http.PostJSON(fmt.Sprintf(qrCodeUnlimited, token), options)
 	if err != nil {
 		return
 	}
-	defer resp.Body.Close()
-	content, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return
-	}
-	err = json.Unmarshal(content, &r)
+	err = json.Unmarshal(resp, &r)
 	if err == nil {
 		return
 	} else {
-		r.Content = content
+		r.Content = resp
 		err = nil
 	}
 	return
