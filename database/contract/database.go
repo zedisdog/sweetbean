bc702810085a6d1257af74e15fc5d0316b68c280
zed <zedsmail@163.com>
2022-07-23 14:34:46 +0800

    feat: database tool interface

---
+++ database/contract/database.go
@@ -0,0 +1,33 @@
+package contract
+
+import "github.com/zedisdog/sweetbean/database"
+
+type CanUseTx[Tx any, Self any] interface {
+	WithTx(Tx) Self
+}
+
+type CanMakeTx[Tx any] interface {
+	Transaction(func(Tx) error) error
+}
+
+//Transaction 事务接口
+type Transaction[Tx any, Self any] interface {
+	CanUseTx[Tx, Self]
+	CanMakeTx[Tx]
+}
+
+//Repo repo基础方法接口
+type Repo[Model any, IdentityType any] interface {
+	Create(*Model) error
+	Update(*Model) error
+	Delete(IdentityType) error
+	First(...database.Condition) (Model, error)
+}
+
+type CanFind[Model any] interface {
+	Find(...database.Condition) ([]Model, error)
+}
+
+type CanPage[Model any] interface {
+	Page(offset, limit int, conditions ...database.Condition) (list []Model, total int, err error)
+}
--- database/tools.go
+++ database/tools.go
@@ -9,9 +9,51 @@ import (
 	"gorm.io/gorm"
 )
 
-type Condition = []interface{}
-type Conditions = []Condition
+type Condition []interface{}
+type Conditions []Condition
 
+func (cs Conditions) Apply(q *gorm.DB) (query *gorm.DB, err error) {
+	query = q
+	for _, condition := range cs {
+		if s, ok := condition[0].(string); ok && (strings.Contains(strings.ToLower(s), "and") || strings.Contains(strings.ToLower(s), "or")) {
+			query = query.Where(s, condition[1:]...)
+		} else {
+			switch len(condition) {
+			case 0, 1:
+				err = errx.New("condition is invalid", 1)
+				return
+			case 2:
+				v := reflect.ValueOf(condition[1])
+				if v.Kind() == reflect.Slice {
+					query = query.Where(fmt.Sprintf("%s IN ?", condition[0]), condition[1])
+				} else {
+					if s, ok := condition[1].(string); ok && strings.HasPrefix(strings.ToLower(s), "is") {
+						query = query.Where(fmt.Sprintf("%s %s", condition[0], condition[1]))
+					} else {
+						query = query.Where(fmt.Sprintf("%s = ?", condition[0]), condition[1])
+					}
+				}
+			case 3:
+				query = query.Where(fmt.Sprintf("%s %s ?", condition[0], condition[1]), condition[2])
+			case 4:
+				switch strings.Replace(strings.ToUpper(condition[1].(string)), " ", "", -1) {
+				case "BETWEEN", "NOTBETWEEN":
+					query = query.Where(fmt.Sprintf("%s %s ? AND ?", condition[0], condition[1]), condition[2], condition[3])
+				default:
+					err = errx.New("condition is invalid", 1)
+					return
+				}
+			default:
+				err = errx.New("condition is invalid", 1)
+				return
+			}
+		}
+	}
+	return
+}
+
+//ParseConditionGorm
+//Deprecated: use Conditions.Parse instead
 func ParseConditionGorm(q *gorm.DB, conditions Conditions) (query *gorm.DB, err error) {
 	// fmt.Printf("%+v\n", conditions)
 	query = q
