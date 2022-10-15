06797e60f5c6af441384b87936355697d90ca569
zed <zedsmail@163.com>
2022-10-15 20:09:57 +0800

    feat: 微信公众号用户信息接口

--- sdk/wechat/official/client.go
+++ sdk/wechat/official/client.go
@@ -1,6 +1,9 @@
 package official
 
-import "github.com/zedisdog/sweetbean/sdk/wechat/official/oauth"
+import (
+	"github.com/zedisdog/sweetbean/sdk/wechat/official/oauth"
+	"github.com/zedisdog/sweetbean/sdk/wechat/official/sns"
+)
 
 func NewClient(appID string, secret string) *Client {
 	return &Client{
@@ -13,6 +16,7 @@ type Client struct {
 	appID  string
 	secret string
 	oauth  *oauth.Oauth
+	sns    *sns.Sns
 }
 
 func (c Client) Oauth() *oauth.Oauth {
@@ -20,4 +24,11 @@ func (c Client) Oauth() *oauth.Oauth {
 		c.oauth = oauth.NewOauth(c.appID, c.secret)
 	}
 	return c.oauth
+}
+
+func (c Client) Sns() *sns.Sns {
+	if c.sns == nil {
+		c.sns = sns.NewSns()
+	}
+	return c.sns
 }
