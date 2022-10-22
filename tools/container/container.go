6c72a572e905aafff5dfb83f36f9a27b6f3e2eb6
zed <zedsmail@163.com>
2022-10-22 16:35:16 +0800

    bug: container

--- tools/container/container.go
+++ tools/container/container.go
@@ -14,7 +14,7 @@ func Get[T any]() T {
 	//先获取泛型的类型
 	var t *T
 	tt := reflect.TypeOf(t).Elem()
-
+	//FIXME: 设a b 两个接口， b接口集成a接口，c d 对象分别继承 a b 两个接口，则此时取a接口的对象 c 和 d 都有可能取到
 	//看map缓存里面有没有，有就返回
 	if instance, ok := instances.Load(tt); ok {
 		return instance.(T)
