fdb34cabe9c2965e53bd71b9d69a2105a9e59542
zed <zedsmail@163.com>
2022-11-17 09:58:15 +0800

    feat: slice group

--- tools/slice.go
+++ tools/slice.go
@@ -1,11 +1,30 @@
 package tools
 
+import "math"
+
 func InSlice[T comparable](x T, s []T) (result int) {
 	result = -1
 	for index, i := range s {
 		if i == x {
 			result = index
 			break
+		}
+	}
+
+	return
+}
+
+func GroupSlice[T any](s []T, groupCount int) (result [][]T) {
+	result = make([][]T, int(math.Ceil(float64(len(s))/float64(groupCount))))
+	index := 0
+
+	for i, d := range s {
+		if result[index] == nil {
+			result[index] = make([]T, 0, groupCount)
+		}
+		result[index] = append(result[index], d)
+		if (i+1)%groupCount == 0 {
+			index++
 		}
 	}
 
